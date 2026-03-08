83d5650c4ed23e0731c954fb2d09b36e934ac1cb
Pablo Stafforini <pablo@stafforini.com>
2026-03-08 12:10:39 -0300

    Update .dir-locals.el to export texinfo instead of info

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,6 @@
 ((org-mode . ((eval . (add-hook 'after-save-hook
                                 (lambda ()
-                                  (when (equal (file-name-nondirectory buffer-file-name)
-                                               "README.org")
-                                    (require 'ox-texinfo)
-                                    (let ((inhibit-message t))
-                                      (org-texinfo-export-to-info))))
+                                  (require 'ox-texinfo)
+                                  (let ((inhibit-message t))
+                                    (org-texinfo-export-to-texinfo)))
                                 nil t)))))
